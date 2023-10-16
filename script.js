6f5ba6d27153d0e446ff43b2561bb7f91e1c7098
ZulAnwar76 <146795472+ZulAnwar76@users.noreply.github.com>
2023-10-17 00:03:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const hireMeButton = document.querySelector('.btn-box a:first-child');
+
+hireMeButton.addEventListener('click', function() {
+    alert("Terima kasih, saya akan lakukan yang terbaik");
+});
+
+const letsTalkButton = document.querySelector('.btn-box a:last-child');
+
+letsTalkButton.addEventListener('click', function() {
+    alert("Boleh di kepoin sosmed saya ya ;)");
+});
